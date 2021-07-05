3f08093499f1b03b62f04f2b72c7e41d29a8d928
Gavin Bell <gavin.bell@kainos.com>
2021-07-05 16:06:18 +0100

    Set up Selenium

---
+++ src/test/java/UnitTest/test.java
@@ -0,0 +1,15 @@
+package UnitTest;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class test {
+
+    @Test
+    public void test() {
+        int answer = 5;
+        assertTrue(answer == 5);
+    }
+}
