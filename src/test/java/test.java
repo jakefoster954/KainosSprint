1fbf11f22341cad333150d2530a8b554c8ec2991
Gavin Bell <gavin.bell@kainos.com>
2021-07-05 15:28:30 +0100

    Set up JUnit

---
+++ src/test/java/test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class test {
+
+    @Test
+    public void test() {
+        int answer = 5;
+        assertTrue(answer == 5);
+    }
+}
