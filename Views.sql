610d3265c82190476d5001f27426ad34acf92cf4
Lauren <lauren.danton@kainos.com>
2021-07-06 15:53:33 +0100

    Insert updated, Views added for use case 1-4

---
+++ Views.sql
@@ -0,0 +1,11 @@
+CREATE VIEW ListJobRoles AS
+SELECT jobName AS 'Job Role' FROM JobRole ORDER BY bandLevelID DESC;
+
+CREATE VIEW JobSpecification AS
+SELECT jobName AS 'Job Role', jobSpec AS 'Job Spec Summary', jobURL AS 'SharePoint Link' FROM JobRole;
+
+CREATE VIEW RoleCapability AS
+SELECT jobName AS 'Job Role', capabilityName AS 'Capability' FROM Capability, JobRole JOIN JobFamily ON JobFamily.jobFamilyID = JobRole.jobFamilyID WHERE JobFamily.capabilityID = Capability.capabilityID;
+
+CREATE VIEW RoleBandLevel AS
+SELECT jobName AS 'Job Role', BandLevel.bandName AS 'Band Level' FROM JobRole, BandLevel WHERE BandLevel.bandLevelID = JobRole.BandLevelID;
