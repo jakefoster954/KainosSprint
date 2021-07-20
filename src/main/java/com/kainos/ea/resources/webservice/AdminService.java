51cee7b01d0ae9186395d5fb5900e45cae1524eb
Barry Bell <gavin.bell@kainos.com>
2021-07-20 16:20:01 +0100

    Fix requested changes

--- src/main/java/com/kainos/ea/resources/webservice/AdminService.java
+++ src/main/java/com/kainos/ea/resources/webservice/AdminService.java
@@ -2,7 +2,14 @@ package com.kainos.ea.resources.webservice;
 
 import com.codahale.metrics.annotation.Timed;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.core.MediaType;
 
 import com.kainos.ea.DTO;
@@ -25,7 +32,7 @@ public class AdminService extends WebService {
      * @param jobRoleName The job you wish to delete
      * @return Status 200. OK if deleting succeeds. Status 500. Internal Server Error otherwise.
      * @throws SQLException Invalid SQL syntax
-     * @throws IOException  Create connection to database.
+     * @throws IOException Create connection to database.
      */
     @DELETE
     @Timed
@@ -47,7 +54,7 @@ public class AdminService extends WebService {
      * @param job An instance of the Job class containing all the data requested by the job class.
      * @return Status 200. OK if adding succeeds. Status 500. Internal Server Error otherwise.
      * @throws SQLException Invalid SQL syntax
-     * @throws IOException  Create connection to database.
+     * @throws IOException Create connection to database.
      */
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
@@ -68,7 +75,7 @@ public class AdminService extends WebService {
      * @param job The job you wish to edit.
      * @return Status 200. OK if editing succeeds. Status 500. Internal Server Error otherwise.
      * @throws SQLException Invalid SQL syntax
-     * @throws IOException  Create connection to database.
+     * @throws IOException Create connection to database.
      * @deprecated This will not probably be implemented in fronted till the end of project
      */
     @PUT
@@ -90,7 +97,7 @@ public class AdminService extends WebService {
      * @param capability An instance of the Capability class containing all the data requested by the capability class.
      * @return Status 200. OK if adding succeeds. Status 500. Internal Server Error otherwise.
      * @throws SQLException Invalid SQL syntax
-     * @throws IOException  Create connection to database.
+     * @throws IOException Create connection to database.
      */
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
@@ -111,7 +118,7 @@ public class AdminService extends WebService {
      * @param capabilityName The capability you wish to delete
      * @return Status 200. OK if deleting succeeds. Status 500. Internal Server Error otherwise.
      * @throws SQLException Invalid SQL syntax
-     * @throws IOException  Create connection to database.
+     * @throws IOException Create connection to database.
      */
     @DELETE
     @Timed
