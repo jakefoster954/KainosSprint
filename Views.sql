267ca3af1874e5d924f011cbadf83381a1286ccb
Lauren <lauren.danton@kainos.com>
2021-07-21 15:39:08 +0100

    Changes to the database

--- Views.sql
+++ Views.sql
@@ -18,3 +18,33 @@ FROM (((JobRole
 INNER JOIN JobFamily ON JobFamily.jobFamilyID = JobRole.jobFamilyID)
 INNER JOIN Capability ON Capability.capabilityID = JobFamily.capabilityID)
 INNER JOIN BandLevel ON BandLevel.bandLevelID = JobRole.bandLevelID);
+
+
+SELECT Capability.capabilityID,  familyName FROM Capability, JobFamily WHERE Capability.capabilityID = JobFamily.capabilityID;
+
+CREATE VIEW JobNamesView AS
+SELECT jobName, capabilityName, bandName
+FROM (((JobRole
+INNER JOIN JobFamily ON JobFamily.jobFamilyID = JobRole.jobFamilyID)
+INNER JOIN Capability ON Capability.capabilityID = JobFamily.capabilityID)
+INNER JOIN BandLevel ON BandLevel.bandLevelID = JobRole.bandLevelID);
+
+CREATE VIEW FullJobDataView AS
+SELECT jobName, jobSpec, jobURL, capabilityName, bandName
+FROM (((JobRole
+INNER JOIN JobFamily ON JobFamily.jobFamilyID = JobRole.jobFamilyID)
+INNER JOIN Capability ON Capability.capabilityID = JobFamily.capabilityID)
+INNER JOIN BandLevel ON BandLevel.bandLevelID = JobRole.bandLevelID);
+
+CREATE VIEW CapabilitiesView AS
+SELECT capabilityName, CapabilityLead.leadName
+FROM (Capability INNER JOIN CapabilityLead ON Capability.leadID = CapabilityLead.leadID);
+
+CREATE VIEW FullCapabilityDataView AS
+SELECT capabilityName, CapabilityLead.leadName, CapabilityLead.leadMessage, CapabilityLead.leadPhoto
+FROM (Capability INNER JOIN CapabilityLead ON Capability.leadID = CapabilityLead.leadID);
+
+"SELECT capabilityName, leadName, leadMessage, leadPhoto " +
+        "FROM Capability " +
+        "WHERE leadName = ?;");
+
